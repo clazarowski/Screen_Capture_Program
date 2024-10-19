83d186bcdf5f4caea00b7317617e4a14a2297581
Cyprian Lazarowski <cyprian.lazarowski@gmail.com>
2024-10-19 11:15:21 +0200

    add hotkey f9 to capture screen add hotkey esc to close program

--- screen_capture.py
+++ screen_capture.py
@@ -1,6 +1,13 @@
 import sys
+import os
+import keyboard
 from PIL import ImageGrab
 from datetime import datetime
+
+def create_folder_if_not_exists():
+    if not os.path.exists("screenshots"):
+        os.makedirs("screenshots")
+        print("Folder screenshots create")
 
 def screen_capture():
     #Capture Screen
@@ -8,10 +15,14 @@ def screen_capture():
 
     #Screen save
     timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-    filename = f"screenshots\\{timestamp}.png"
+    filename = f"screenshots/{timestamp}.png"
     screenshot.save(filename)
 
     #Close Screen capture
     screenshot.close()
+    print(f"Screenshot save as {filename}")
 
-screen_capture()
+create_folder_if_not_exists()
+keyboard.add_hotkey('F9', screen_capture)
+keyboard.wait('esc')
+print("Program Close")
