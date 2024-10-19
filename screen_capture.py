4bdfdeecd2064191440317ea329c1d4541641bd7
Cyprian Lazarowski <cyprian.lazarowski@gmail.com>
2024-10-19 12:15:31 +0200

    change lib pillow to mss

--- screen_capture.py
+++ screen_capture.py
@@ -1,28 +1,26 @@
-import sys
 import os
 import keyboard
-from PIL import ImageGrab
+import mss
 from datetime import datetime
 
 def create_folder_if_not_exists():
     if not os.path.exists("screenshots"):
         os.makedirs("screenshots")
-        print("Folder screenshots create")
+        print("Folder screenshots created")
 
 def screen_capture():
-    #Capture Screen
-    screenshot = ImageGrab.grab()
+    with mss.mss() as sct:
+        monitor = sct.monitors[2]  # 0 - all, 1 - first monitor, 2 - second monitor 
+        screenshot = sct.grab(monitor)
 
-    #Screen save
-    timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-    filename = f"screenshots/{timestamp}.png"
-    screenshot.save(filename)
+        # Save screeen
+        timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        filename = f"screenshots/{timestamp}.png"
+        mss.tools.to_png(screenshot.rgb, screenshot.size, output=filename)
 
-    #Close Screen capture
-    screenshot.close()
-    print(f"Screenshot save as {filename}")
+        print(f"Screenshot saved as {filename}")
 
 create_folder_if_not_exists()
 keyboard.add_hotkey('F9', screen_capture)
 keyboard.wait('esc')
-print("Program Close")
+print("Program closed")
